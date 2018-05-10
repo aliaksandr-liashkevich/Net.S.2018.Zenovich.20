aafde32c108f1b944621dfef2c5c62935326b6fb
azenovich <alexander.zenovich@gmail.com>
2018-05-10 22:35:55 +0300

    Добавьте файлы проекта.

---
+++ Net.S.2018.Zenovich.20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.S._2018.Zenovich._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
