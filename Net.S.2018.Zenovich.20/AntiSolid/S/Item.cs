bf2201229194886089726513c4566a393391071b
azenovich <alexander.zenovich@gmail.com>
2018-05-10 22:41:47 +0300

    S was added.

---
+++ Net.S.2018.Zenovich.20/AntiSolid/S/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.S._2018.Zenovich._20.AntiSolid.S
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int Price;
+
+        public bool IsValid(bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                return Price > 0;
+            }
+
+            return Price > 100;
+        }
+    }
+}
